24127da639034ed766a46030748889e1fd11e270
yuval <Arbelyuval56@gmail.com>
2022-12-01 17:08:06 +0200

    Getting the patch from the recived data

--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+
 import socket
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
@@ -6,8 +7,13 @@ server.listen(5)
 while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
-    data = client_socket.recv(100)
+    data = client_socket.recv(100).decode()
     print('Received: ', data)
-    client_socket.send(data.upper())
+    dataList = data.split(' ')
+    i = 2
+    patchFile = dataList[1]
+    while dataList[i] != 'HTTP/1.1\r\nHost:':
+        patchFile += dataList[i]
+    print(patchFile)
     client_socket.close()
-    print('Client disconnected')
+    print('Client disconnected\n')
