0bece5ea2be2a67c9ae399e7ce583a109d78faa9
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2022-12-01 16:57:29 +0200

    add main func

--- server.py
+++ server.py
@@ -2,7 +2,6 @@ import socket
 import os
 import sys
 
-
 # Opening the server's socket.
 s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 # Getting the port from the system.
@@ -13,16 +12,16 @@ if not server_port.isnumeric() or (int(server_port) not in range(0, 65536)):
 # Binding the server port (received from the sys).
 s.bind(('', int(server_port)))
 
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
 server.listen(5)
 
-while True:
-    client_socket, client_address = server.accept()
-    print('Connection from: ', client_address)
-    data = client_socket.recv(100)
-    print('Received: ', data.decode())
-    client_socket.send(data)
-    client_socket.close()
-    print('Client disconnected')
+if __name__ == '__main__':
+    while True:
+        client_socket, client_address = server.accept()
+        print('Connection from: ', client_address)
+        data = client_socket.recv(100)
+        print('Received: ', data.decode())
+        client_socket.send(data)
+        client_socket.close()
+        print('Client disconnected')
