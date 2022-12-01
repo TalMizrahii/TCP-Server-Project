dbab9e7c4099213ec27975d6e19429dcad37b471
TALSPC\Tal Mizrahi <Taltalon1927@gmail.com>
2022-12-01 16:49:48 +0200

    added server's sys argument

--- server.py
+++ server.py
@@ -1,5 +1,19 @@
 import socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+import os
+import sys
+
+# Opening the server's socket.
+s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# Getting the port from the system.
+server_port = str(sys.argv[1])
+# If the port is not valid, exit the program.
+if not server_port.isnumeric() or (int(server_port) not in range(0, 65536)):
+    exit(0)
+# Binding the server port (received from the sys).
+s.bind(('', int(server_port)))
+
+
+server = socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(('', 12345))
 server.listen(5)
 
@@ -7,7 +21,7 @@ while True:
     client_socket, client_address = server.accept()
     print('Connection from: ', client_address)
     data = client_socket.recv(100)
-    print('Received: ', data)
-    client_socket.send(data.upper())
+    print('Received: ', data.decode())
+    client_socket.send(data)
     client_socket.close()
     print('Client disconnected')
