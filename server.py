da8e2df887f11c08c87e01c90c4b514a3e50119f
yuval <arbelyuval56@gmail.com>
2022-12-11 15:13:17 +0200

    changed timeout

--- server.py
+++ server.py
@@ -10,19 +10,15 @@ import os
 import sys
 
 
-# # Opening the server's socket.
-# server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# # Getting the port from the system.
-# server_port = str(sys.argv[1])
-# # If the port is not valid, exit the program.
-# if not server_port.isnumeric() or (int(server_port) not in range(0, 65536)):
-#     exit(0)
-# # Binding the server port (received from the sys).
-# server.bind(('', int(server_port)))
-# server.listen(5)
-
+# Opening the server's socket.
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('', 12345))
+# Getting the port from the system.
+server_port = str(sys.argv[1])
+# If the port is not valid, exit the program.
+if not server_port.isnumeric() or (int(server_port) not in range(0, 65536)):
+    exit(0)
+# Binding the server port (received from the sys).
+server.bind(('', int(server_port)))
 server.listen(5)
 
 
@@ -122,7 +118,7 @@ def send_to_client(sock):
 def main():
     while True:
         client_socket, client_address = server.accept()
-        client_socket.settimeout(10.0)
+        client_socket.settimeout(1.0)
         send_to_client(client_socket)
         client_socket.close()
 
